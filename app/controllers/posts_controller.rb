d03410411a965dcafafb2197bf54b089a0f9cd07
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-04-30 03:15:06 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,21 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
+
+  def new
+    @post= Post.new
+  end
+
+  def create
+    @post= Post.create!(title: params[:post][:title], description: params[:post][:description])
+    redirect_to post_path(@post)
+  end
+
+
+  def show
+    @post= Post.find(params[:id])
+  end
+
 end
+
