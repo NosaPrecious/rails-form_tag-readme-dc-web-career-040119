527f44c3b3db318b6f49aec91c397082b09bef21
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-04-30 03:17:30 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe 'new post' do
   it 'ensures that the form route works with the /new action' do
-    byebug
     visit new_post_path
     expect(page.status_code).to eq(200)
   end
