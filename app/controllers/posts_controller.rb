a46b97ef67cbec1a489e25c144b88068b493f7a8
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-04-30 03:20:58 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,21 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
+
+  def new
+    @post= Post.new
+  end
+
+  def create
+    @post= Post.create!(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
+
+  def show
+    @post= Post.find(params[:id])
+  end
+
 end
+
